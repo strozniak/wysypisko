f501567bc71aa757343df4f8d59650c83c907c38
Piotr Strozniak <piotrek.strozniak@gmail.com>
2011-05-04 17:50:47 +0200

    we meet again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wysypisko::Application.config.secret_token = '9d121dfe08ca8322f3bafed3cbec989d308691dbfad7b9c11b5120571a80c6da77f31587785af4a528192dc71e8d4bc1148d7b1eea9316485f37b860508e0501'
