e990b060cb6a2368127e4f078621f595b0c132ff
Piotr Strozniak <piotrek.strozniak@gmail.com>
2011-05-05 21:08:37 +0200

    skonczylem 4 rozdzial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,26 @@
 class PagesController < ApplicationController
   def main
+    @title = "Glowna"
   end
 
   def add
+    @title = "Dodaj zawartosc"
   end
 
   def register
+    @title = "Rejestracja"
   end
 
   def top
+    @title = "Top"
   end
 
   def random
+    @title = "Losowa"
   end
 
   def stats
+    @title = "Statystyki"
   end
 
 end
