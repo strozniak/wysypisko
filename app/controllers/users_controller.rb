5aa15f27abc0f867e7a9e77a29a43f62ad3a32a0
Piotr Strozniak <piotrek.strozniak@gmail.com>
2011-06-01 03:16:17 +0200

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,21 @@ class UsersController < ApplicationController
   def new
     @title = "Sign up"
   end
-  
+
+  def new
+    @user = User.new
+    @title = "Rejestracja"
+  end
+ 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+        flash[:success] = "Wytamy na wysypisku!"
+        redirect_to @user
+   else
+       @title = "Rejestracja"
+       render 'new'
+   end
+ end 
 end
 
