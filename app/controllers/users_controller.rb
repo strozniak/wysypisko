bbc029229d078d1145f3c6d879e264eb4c0d2e59
Piotr Strozniak <piotrek.strozniak@gmail.com>
2011-06-14 01:30:36 +0200

    Skonczylem wszystko zwiazane z modelem - usuwanie/updateowanie, indeksy i itd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
+
+  def index
+    @title = "Wszyscy uzytkownicy"
+    @users = User.paginate(:page => params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -27,5 +36,43 @@ class UsersController < ApplicationController
        render 'new'
    end
  end 
+
+ def edit
+   @title = "Edycja uzytkownika"
+ end 
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "	Profil uaktualniony."
+      redirect_to @user
+    else
+      @title = "Edytuj uzytkownika"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Uzytkownik usuniety."
+    redirect_to users_path
+  end
+
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user= User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
 end
 
